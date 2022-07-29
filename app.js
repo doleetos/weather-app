dae95f291fea1d36f46083641f5e2343cf7283f6
doleetos <104799971+doleetos@users.noreply.github.com>
2022-07-28 22:23:20 -0700

    added comments

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// get HTML elements
 const result = document.getElementById("result");
 const result2 = document.getElementById("result2");
 const result3 = document.getElementById("result3");
@@ -6,6 +8,62 @@ const searchBox = document.getElementById("search-box");
 const greetings = document.getElementById('greeting');
 const mainBody = document.querySelector('body');
 
+
+// loads and displays data 
+window.addEventListener('load', getWeather);
+timeOfDay();
+searchBtn.addEventListener('click', getWeather);
+searchBox.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        getWeather()
+    }
+});
+
+
+// fetch weather API data and display contents on app
+function getWeather() {
+    let cityValue = searchBox.value;
+    if (cityValue == 0) {
+        alert('Please enter a city name.');
+    } else {
+        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&appid=${key}&units=imperial`;
+        searchBox.value = "";
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((data) => {
+                result.innerHTML = `<h1 class="city-name">${data.city.name} <span>Today</span></h1>
+                                    <img class="weather-icon" src="https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@4x.png" />
+                                    <h1 class="temp">${parseInt(data.list[0].main.temp)} °F</h1>
+                                    <h3 class="desc">${data.list[0].weather[0].description}</h3>
+                                    `;
+                result2.innerHTML = `<h1 class="city-name">${data.city.name} <span>${timeConverter(data.list[7].dt)}</span></h1>
+                                    <img class="weather-icon" src="https://openweathermap.org/img/wn/${data.list[7].weather[0].icon}@4x.png" />
+                                    <h1 class="temp">${parseInt(data.list[7].main.temp)} °F</h1>
+                                    <h3 class="desc">${data.list[7].weather[0].description}</h3>
+                                    `;
+                result3.innerHTML = `<h1 class="city-name">${data.city.name} <span>${timeConverter(data.list[15].dt)}</span></h1>
+                                    <img class="weather-icon" src="https://openweathermap.org/img/wn/${data.list[15].weather[0].icon}@4x.png" />
+                                    <h1 class="temp">${parseInt(data.list[15].main.temp)} °F</h1>
+                                    <h3 class="desc">${data.list[15].weather[0].description}</h3>
+                                    `;
+            })
+            .catch(() => { alert('City not found.'); });
+    }
+}
+
+
+// converts UNIX timestamp to regular date in month day format
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var time = month + ' ' + date ;
+    return time;
+}
+
+
+// gets user's current time to change background color
 function timeOfDay() {
     const d = new Date();
     let hour = d.getHours();
@@ -17,53 +75,6 @@ function timeOfDay() {
         mainBody.style.background = 'linear-gradient(#424874, #5E6073)';
     }
 }
-timeOfDay()
-
-let getWeather = () => {
-    let cityValue = searchBox.value;
-    if(cityValue == 0) {
-        alert('Please enter a city name.');
-    } else {
-        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&appid=${key}&units=imperial`;
-        searchBox.value="";
-        fetch(url)
-            .then((resp) => resp.json())
-            .then((data) => {
-                result.innerHTML = `<h1 class="city-name">${data.city.name} <span>Today</span></h1>
-                                    <img class="weather-icon" src="https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@4x.png" />
-                                    <h1 class="temp">${parseInt(data.list[0].main.temp)} °F</h1>
-                                    <h3 class="desc">${data.list[0].weather[0].description}</h3>
-                                    `; 
-                result2.innerHTML = `<h1 class="city-name">${data.city.name} <span>${timeConverter(data.list[7].dt)}</span></h1>
-                                    <img class="weather-icon" src="https://openweathermap.org/img/wn/${data.list[7].weather[0].icon}@4x.png" />
-                                    <h1 class="temp">${parseInt(data.list[7].main.temp)} °F</h1>
-                                    <h3 class="desc">${data.list[7].weather[0].description}</h3>
-                                    `;
-                result3.innerHTML = `<h1 class="city-name">${data.city.name} <span>${timeConverter(data.list[15].dt)}</span></h1>
-                                    <img class="weather-icon" src="https://openweathermap.org/img/wn/${data.list[15].weather[0].icon}@4x.png" />
-                                    <h1 class="temp">${parseInt(data.list[15].main.temp)} °F</h1>
-                                    <h3 class="desc">${data.list[15].weather[0].description}</h3>
-                                    `;                              
-            })                      
-            .catch(() => {alert('City not found.');});
-        }
-}
-searchBtn.addEventListener('click', getWeather);
-searchBox.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        getWeather()
-    }
-});
-window.addEventListener('load', getWeather);
 
 
 
-function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var time = month + ' ' + date ;
-    return time;
-}
